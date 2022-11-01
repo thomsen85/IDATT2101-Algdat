91a91e6fead8f497882e4785031f3338c4158a13
Thomas <thomassvendal@gmail.com>
2022-11-01 11:07:09 +0100

    Restrcutured the code

--- practice-8-compression/rust_compression/src/main.rs
+++ practice-8-compression/rust_compression/src/main.rs
@@ -11,12 +11,153 @@ static SEARCH_WINDOW_BITS: u8 = 15; // 11 for backref: 2^15 = 32768
 static LOOK_AHEAD_BITS: u8 = 9; // 5 for looka ahead: 2^9 = 512
 static DISTANCE_BITS: u8 = 8; // 8 for distance unitl next : 2^8 = 256
 
+/// Tree struct for holding the huffman tree, using lookuptables to increase performace.
+#[derive(Debug)]
+struct Tree {
+    root_node: Node,
+    lookup_table: Vec<Vec<bool>>,
+}
+
+impl Tree {
+    fn new(root_node: Node) -> Self {
+        let mut lookup_table = vec![vec![]; 256];
+        Tree::fill_lookup_table(&root_node, &mut lookup_table, Vec::new());
+        Self { root_node, lookup_table }
+    }
+
+    fn fill_lookup_table(node: &Node, lookup_table: &mut Vec<Vec<bool>>, progress: Vec<bool>) {
+        if let Some(val) = node.value {
+            lookup_table[val as usize] = progress.clone();
+        } else {
+            if let Some(l_node) = &node.left {
+                let mut l_progress = progress.clone();
+                l_progress.push(false);
+                Tree::fill_lookup_table(&Rc::clone(l_node).as_ref().borrow(), lookup_table, l_progress);
+            }
+            if let Some(r_node) = &node.right {
+                let mut r_progress = progress.clone();
+                r_progress.push(true);
+                Tree::fill_lookup_table(&Rc::clone(r_node).as_ref().borrow(), lookup_table, r_progress)
+            }
+        }     
+    }
+
+    fn get_encoded_bit_array_from_byte(&self, index: u8) -> &Vec<bool> {
+        &self.lookup_table[index as usize]
+    }
+
+    fn get_decode_bytes(&self, encoded_bytes: &Vec<u8>, start: usize) -> Vec<u8> {
+        let mut buff = Vec::new();
+        let mut res = Vec::new();
+        let last_active_bits = encoded_bytes.last().unwrap();
+        let stop =  ((encoded_bytes.len() - 1) * 8) - (7  - last_active_bits.to_owned() as usize);
+        
+        for i in start..stop {
+            buff.push(get_bit(encoded_bytes, i));
+
+            if self.lookup_table.contains(&buff) {
+                res.push(self.lookup_table.iter().position(|o|  o == &buff).unwrap() as u8);
+                buff.clear();
+            }
+        }
+        res
+    }
+
+}
+
+#[derive(Debug, PartialEq, Eq)]
+struct Node {
+    left: Option<Rc<RefCell<Node>>>,
+    right: Option<Rc<RefCell<Node>>>,
+    frequency: u32,
+    value: Option<u8>, 
+}
+
+impl Node {
+    fn new_bottom_node(frequency: u32, value: Option<u8>) -> Self{
+        Self { left: None, right: None, frequency, value }
+    }
+
+    fn new_intermidiate_node(left: Node, right: Node) -> Self {
+        let freq = left.frequency + right.frequency;
+        Self {left: Some(Rc::new(RefCell::new(left))), right: Some(Rc::new(RefCell::new(right))), frequency: freq, value: None }
+    }
+
+}
+
+impl Ord for Node{
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.frequency.cmp(&other.frequency).reverse()
+    }
+}
+
+impl PartialOrd for Node {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+struct BitBuilder {
+    bytes: Vec<u8>, 
+    buffer_byte: u8,
+    bit_pos: u8,
+}
+
+impl BitBuilder {
+    fn new() -> Self {
+        Self { bytes:Vec::new(), buffer_byte: 0, bit_pos: 0}
+    }
+
+    fn extend(&mut self, bits: &Vec<bool>) {
+        for bit in bits {
+            if self.bit_pos >= 8 {
+                self.bit_pos = 0;
+                self.bytes.push(self.buffer_byte);
+                self.buffer_byte = 0; 
+            } 
+            
+            if *bit {
+                self.buffer_byte |= 1 << (7 - self.bit_pos)
+            }
+
+            self.bit_pos += 1
+        }
+    }
+
+    fn add_byte(&mut self, byte: u8) {
+        self.bytes.push(byte);
+    }
+
+    fn add_u32(&mut self, input: u32) {
+        let b1: u8 = ((input >> 24) & 0xff) as u8;
+        let b2: u8 = ((input >> 16) & 0xff) as u8;
+        let b3: u8 = ((input >> 8) & 0xff) as u8;
+        let b4: u8 = (input & 0xff) as u8;
+
+        self.add_byte(b1);
+        self.add_byte(b2);
+        self.add_byte(b3);
+        self.add_byte(b4);
+    }
+
+    fn collect(mut self) -> Vec<u8> {
+        let rest = self.buffer_byte;
+        self.bytes.push(rest);
+        self.bytes.push(self.bit_pos as u8);
+        self.bytes
+    }
+}
+
+
+
 fn get_indicator_from_data(back_ref: u32, length: u32, distance_to_next: u32) -> [u8; INDICATOR_BYTE] {
     let mut num: u32 = 0x00;
     
     assert!(distance_to_next < 2_u32.pow(DISTANCE_BITS as u32));
     assert!(length < 2_u32.pow(LOOK_AHEAD_BITS as u32));
     assert!(back_ref < 2_u32.pow(SEARCH_WINDOW_BITS as u32), "backref = {}",  back_ref);
+    if length > 2_u32.pow(LOOK_AHEAD_BITS as u32) {
+        println!("{}", length);
+    }
 
     num |= distance_to_next; 
     num |= length << DISTANCE_BITS;   
@@ -70,8 +211,11 @@ fn lz_encode(bytes: &Vec<u8>) -> Vec<u8> {
     let mut prev_length = 0; 
 
     while split < bytes.len() {
+        if split % 500 == 0{
+            print!("\r {:.2}%", (split as f64/bytes.len() as f64) * 100.0)
+        }
         let (back_ref, length) = find_match_in_window(bytes, split, SEARCH_WINDOW_BITS, LOOK_AHEAD_BITS);
-        if length > 3 {
+        if length > 4 {
             if first {
                 output.extend(get_byte_array_from_u16(split as u16));
                 first = false; 
@@ -91,6 +235,7 @@ fn lz_encode(bytes: &Vec<u8>) -> Vec<u8> {
         distance += 1;
         buffer_end = split;
     }
+    println!("\r100.00%");
 
     let indicator = get_indicator_from_data(prev_pointer as u32, prev_length, distance);
     output.extend(indicator);
@@ -258,110 +403,8 @@ fn hc_decode(bytes: &Vec<u8>) -> Vec<u8> {
     tree.get_decode_bytes(bytes, (1 + 5*length)*8)
 
 }
-struct BitBuilder {
-    bytes: Vec<u8>, 
-    buffer_byte: u8,
-    bit_pos: u8,
-}
-
-impl BitBuilder {
-    fn new() -> Self {
-        Self { bytes:Vec::new(), buffer_byte: 0, bit_pos: 0}
-    }
-
-    fn extend(&mut self, bits: &Vec<bool>) {
-        for bit in bits {
-            if self.bit_pos >= 8 {
-                self.bit_pos = 0;
-                self.bytes.push(self.buffer_byte);
-                self.buffer_byte = 0; 
-            } 
-            
-            if *bit {
-                self.buffer_byte |= 1 << (7 - self.bit_pos)
-            }
-
-            self.bit_pos += 1
-        }
-    }
-
-    fn add_byte(&mut self, byte: u8) {
-        self.bytes.push(byte);
-    }
-
-    fn add_u32(&mut self, input: u32) {
-        let b1: u8 = ((input >> 24) & 0xff) as u8;
-        let b2: u8 = ((input >> 16) & 0xff) as u8;
-        let b3: u8 = ((input >> 8) & 0xff) as u8;
-        let b4: u8 = (input & 0xff) as u8;
-
-        self.add_byte(b1);
-        self.add_byte(b2);
-        self.add_byte(b3);
-        self.add_byte(b4);
-    }
-
-    fn collect(mut self) -> Vec<u8> {
-        let rest = self.buffer_byte;
-        self.bytes.push(rest);
-        self.bytes.push(self.bit_pos as u8);
-        self.bytes
-    }
-}
-
-#[derive(Debug)]
-struct Tree {
-    root_node: Node,
-    lookup_table: Vec<Vec<bool>>,
-}
-
-impl Tree {
-    fn new(root_node: Node) -> Self {
-        let mut lookup_table = vec![vec![]; 256];
-        Tree::fill_lookup_table(&root_node, &mut lookup_table, Vec::new());
-        Self { root_node, lookup_table }
-    }
-
-    fn fill_lookup_table(node: &Node, lookup_table: &mut Vec<Vec<bool>>, progress: Vec<bool>) {
-        if let Some(val) = node.value {
-            lookup_table[val as usize] = progress.clone();
-        } else {
-            if let Some(l_node) = &node.left {
-                let mut l_progress = progress.clone();
-                l_progress.push(false);
-                Tree::fill_lookup_table(&Rc::clone(l_node).as_ref().borrow(), lookup_table, l_progress);
-            }
-            if let Some(r_node) = &node.right {
-                let mut r_progress = progress.clone();
-                r_progress.push(true);
-                Tree::fill_lookup_table(&Rc::clone(r_node).as_ref().borrow(), lookup_table, r_progress)
-            }
-        }     
-    }
-
-    fn get_encoded_bit_array_from_byte(&self, index: u8) -> &Vec<bool> {
-        &self.lookup_table[index as usize]
-    }
 
 
-    fn get_decode_bytes(&self, encoded_bytes: &Vec<u8>, start: usize) -> Vec<u8> {
-        let mut buff = Vec::new();
-        let mut res = Vec::new();
-        let last_active_bits = encoded_bytes.last().unwrap();
-        let stop =  ((encoded_bytes.len() - 1) * 8) - (7  - last_active_bits.to_owned() as usize);
-        
-        for i in start..stop {
-            buff.push(get_bit(encoded_bytes, i));
-
-            if self.lookup_table.contains(&buff) {
-                res.push(self.lookup_table.iter().position(|o|  o == &buff).unwrap() as u8);
-                buff.clear();
-            }
-        }
-        res
-    }
-
-}
 
 fn get_bit(bytes: &[u8], index: usize) -> bool {
     let byte = bytes[index / 8];
@@ -370,37 +413,6 @@ fn get_bit(bytes: &[u8], index: usize) -> bool {
     byte >> (7 - index) & 1 != 0 
 }
 
-#[derive(Debug, PartialEq, Eq)]
-struct Node {
-    left: Option<Rc<RefCell<Node>>>,
-    right: Option<Rc<RefCell<Node>>>,
-    frequency: u32,
-    value: Option<u8>, 
-}
-
-impl Node {
-    fn new_bottom_node(frequency: u32, value: Option<u8>) -> Self{
-        Self { left: None, right: None, frequency, value }
-    }
-
-    fn new_intermidiate_node(left: Node, right: Node) -> Self {
-        let freq = left.frequency + right.frequency;
-        Self {left: Some(Rc::new(RefCell::new(left))), right: Some(Rc::new(RefCell::new(right))), frequency: freq, value: None }
-    }
-
-}
-
-impl Ord for Node{
-    fn cmp(&self, other: &Self) -> Ordering {
-        self.frequency.cmp(&other.frequency).reverse()
-    }
-}
-
-impl PartialOrd for Node {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(self.cmp(other))
-    }
-}
 
 fn frequency_list_to_huffman_tree(frequency_list: &[(u8, u32)]) -> Tree {
     let mut heap = BinaryHeap::new();
